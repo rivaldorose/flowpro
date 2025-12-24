1def825252687c6431b4c3af2ac5d7d2f9223d37
rivaldorose <mac.valdo1997@gmail.com>
2025-12-24 03:36:31 +0100

    Fix: CSS import order en utils import path in Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { 
   LayoutDashboard, 
   FolderKanban, 
